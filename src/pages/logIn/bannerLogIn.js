e7a094baacc8d475c4d4230a5bb96045d75197f3
Pham Thao Vy <ptvy0812@gmail.com>
2022-11-19 16:44:59 +0700

    final login page

--- src/pages/logIn/bannerLogIn.js
+++ src/pages/logIn/bannerLogIn.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const bannerLogIn = () => {
     return (      
-            <div class=" h-screen flex-1 bg-red-600 bg-primary-600 flex flex-col justify-around ">
-                <h1 class="font-bold text-4xl text-white mx-4  text-center">WELLCOME TO</h1>
-                <h1 class="font-bold text-4xl text-white mx-4  text-center">- HEAVEN OF BOOKS -</h1>
+            <div class=" h-screen flex-1 bg-red-600 bg-primary-600 flex flex-col justify-between px-40 ">
+                <h1 class="font-bold text-4xl text-white mx-4  text-center mt-6">WELCOME TO</h1>
+                <h1 class="font-bold text-4xl text-white mx-4  text-center mt-2 ">- HEAVEN OF BOOKS -</h1>
                 <div>
                     <img  src={require("../../assets/bg_log_in.png")} class="m-auto" />
                 </div>
-                <h4 class="font-pacifico px-20 text-center text-xl italic" >“Good books, like good friends, are few and chosen;the more select, the more enjoyable.”</h4>
+                <h4 class="font-pacifico  text-center text-lg italic mb-16" >“Good books, like good friends, are few and chosen; the more select, the more enjoyable.”</h4>
             </div>
     );
 };
--- src/pages/logIn/logIn.js
+++ src/pages/logIn/logIn.js
@@ -8,9 +8,9 @@ const logIn = () => {
         <div class="bg-slate-100 flex">
             <BannerLogIn/>
             <div class="flex-1 flex flex-col justify-around">
-                <h1 class="text-secondary-gray text-3xl font-bold pl-24 font-mono">Đăng nhập</h1>
+                <h1 class="text-secondary-gray text-3xl font-bold pl-24 mt-24 ml-3.5">Đăng nhập</h1>
                 <FormLogIn/>
-                <Button textBtn="Đăng nhập"/>    
+                <button class="bg-primary  text-center w-1/4 mb-16 ml-44 py-2 rounded-md font-bold text-lg text-white">Đăng nhập</button> 
             </div>
         </div>
     );
