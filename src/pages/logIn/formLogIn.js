5e61d8bbed5516070d6727121a777341f2959589
Pham Thao Vy <ptvy0812@gmail.com>
2022-11-19 15:49:42 +0700

    fix login page

--- src/pages/logIn/formLogIn.js
+++ src/pages/logIn/formLogIn.js
@@ -5,17 +5,23 @@ const formLogIn = () => {
      
     return (
         <form class=" px-28 ">
-            <div class="flex justify-between ">
+            <div >
                 <div>
-                    <label for="username" class="text-secondary-gray text-xl font-medium">Tài khoảng</label>
-                    <input type="text" name="username" placeholder='username' class="bg-amber-50 py-2 w-64 mt-1"></input>
-                    <span class="text-orange-500 text-base"></span>
+                    <label for="username" class="text-secondary-gray text-xl font-medium ">Tài khoảng</label>
+                    <input type="text" name="username" class="bg-amber-50 py-2 w-full mt-1"></input>
+                    <span class="text-orange-500 text-xs font-mono">requered</span>
                 </div>
                 <div>
-                    <label for="password" class="text-secondary-gray text-xl font-medium">Mật khẩu</label>
-                    <input type="text" name="password" placeholder='password' class="bg-amber-50 py-2 w-64 mt-1"></input>
-                    <span class="text-orange-500 text-base"></span>
+                    <label for="password" class="text-secondary-gray text-xl font-medium ">Mật khẩu</label>
+                    <input type="text" name="password" class="bg-amber-50 py-2 w-full mt-1"></input>
+                    <span class="text-orange-500 text-xs font-mono">requered</span>
                 </div>
+                <div class="flex text-xs underline-offset-2 ">
+                    <a href='/src/pages/signUp/signUp.js' >Quên mật khẩu</a>
+                    <p >/</p>
+                    <a href='/src/pages/signUp/signUp.js' >Đăng ký</a>
+                </div>
+               
             </div>  
         </form>
         
--- src/pages/logIn/logIn.js
+++ src/pages/logIn/logIn.js
@@ -8,9 +8,8 @@ const logIn = () => {
         <div class="bg-slate-100 flex">
             <BannerLogIn/>
             <div class="flex-1 flex flex-col justify-around">
-                <h1 class="text-secondary-gray text-3xl font-bold pl-24">Đăng nhập</h1>
+                <h1 class="text-secondary-gray text-3xl font-bold pl-24 font-mono">Đăng nhập</h1>
                 <FormLogIn/>
-                <a >Đăng ký</a>
                 <Button textBtn="Đăng nhập"/>    
             </div>
         </div>
