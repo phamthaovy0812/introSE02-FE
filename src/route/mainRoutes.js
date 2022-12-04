0c023871a00f61a9d0af3de05a6ee5f9e250a372
Pham Thao Vy <ptvy0812@gmail.com>
2022-12-04 22:32:47 +0700

    full fe source code

--- src/route/mainRoutes.js
+++ src/route/mainRoutes.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/home/home";
-import LogIn from "../pages/logIn/logIn";
 import SignUp from "../pages/signUp/signUp";
 import Ranking from "../pages/ranking/ranking";
+
+import LogIn from "../pages/logIn/logIn";
+import Error from "../pages/error/error";
+
+import Search from "../pages/search/search"
+import Profile from "../pages/profile/profile"
+import Newfeed from "../pages/newfeed/newfeed"
+import FullPost from "../pages/fullpost/fullpost"
+
 
 const mainRoutes = () => {
   return (
@@ -12,9 +20,17 @@ const mainRoutes = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="Home" element={<Home />} />
-          <Route path="LogIn" element={<LogIn />} />
           <Route path="SignUp" element={<SignUp />} />
           <Route path="Ranking" element={<Ranking />} />
+
+          <Route path="Error" element={<Error />} />
+          <Route path="LogIn" element={<LogIn />} />
+          <Route path="Search" element={<Search/>} />
+          <Route path="Profile" element={<Profile/>} />
+          <Route path="Newfeed" element={<Newfeed />} />
+          <Route path="FullPost" element={<FullPost/>} />
+
+
         </Routes>
       </BrowserRouter>
     </div>
